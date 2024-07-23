258f1737dbf27e0b65177bcdc6c6632cf9927a38
Thyrum <adj00@protonmail.com>
2024-07-23 16:48:08 +0000

    Add chordpro library for basic chordpro rendering
    I will eventually do this with my own rendering library since this
    library is just vastly inadequate.

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,4 +6,16 @@ class Song < ApplicationRecord
       title
     end
   end
+
+  def html
+    Chordpro.html(body)
+  end
+
+  def chordpro
+    "{title: #{title}}
+{subtitle: #{subtitle}}
+{artist: #{artist}}
+#{body}"
+  end
+
 end
